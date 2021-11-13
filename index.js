96ac2453a195d007f8aa889df7ade85a739a2568
sandy12345231 <kumarisandy29@gmail.com>
2021-11-13 23:36:17 +0530

    git deploy

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("./src/database/db");
 const Routers = require('./src/Routers/Api');
-const cors = require("cors")
+const cors = require("cors");
+// deploy
+const path = require('path');
+require("./src/database/db");
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
@@ -10,9 +14,18 @@ app.use(cors());
 app.use("/api" , Routers);
 
 
-
-
-
-app.listen(3000, ()=>{
-    console.log("Server connected in 3000 port");
+const PORT = process.env.Port || 3000;
+if(process.env.NODE_ENV === 'producation'){
+    app.use(express.static('client/build'));
+}
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+
+app.listen(PORT,()=>{
+    console.log("Server connected in 3000 port");
+})
+
+// app.listen(3000, ()=>{
+//     console.log("Server connected in 3000 port");
+// });
